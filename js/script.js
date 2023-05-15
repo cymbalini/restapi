66712cdb79232a15c047dd71219aac4783f3de0e
Zosia Samosia <Zosia@Samosia.pl>
2023-05-15 13:58:37 +0200

    refractor

--- js/script.js
+++ js/script.js
@@ -1,20 +1,53 @@
 'use strict'
-const getData=()=>{
-    fetch('https://api.nbp.pl/api/exchangerates/tables/a/?format=json')
-    .then(Response => Response.json())
-    .then(result=> {
-        console.log(result)
-        
-        console.log(result[0].rates)
-        result[0].rates.forEach((elem, i) => {
-            const tr = document.createElement('tr')
-            tr.innerHTML=`<td>${++i}</td>
+const baseurl = 'https://api.nbp.pl/api/exchangerates/tables/'
+const getCurrency = async (table) => {
+    try {
+        const Response = await fetch(`${baseurl}${table}`)
+        const data = await Response.json()
+        return data
+    } catch (err) {
+        console.log(`to jest błąd ${err}`)
+    }
+}
+getCurrency('a').then(data => {
+    console.log(data)
+    // const currencies = data[0]
+    // destrukturyzacja
+    const [currencies] = data
+    const { rates } = currencies
+    rates.forEach((elem, i) => {
+        const tr = document.createElement('tr')
+            tr.innerHTML = `<td>${++i}</td>
             <td>${elem.currency}</td>
             <td>${elem.mid}</td>`
             document.querySelector('table tbody').appendChild(tr)
-        });
     })
-}
-const btn=document.querySelector('button')
-btn.addEventListener('click',getData)
+})
+// const getData = () => {
 
+//     fetch(baseurl)
+//         .then(Response => Response.json())
+//         .then(result => {
+//             console.log(result)
+//             console.log(result[0].rates)
+//             result[0].rates.forEach((elem, i) => {
+//                 const tr = document.createElement('tr')
+//                 tr.innerHTML = `<td>${++i}</td>
+//                 <td>${elem.currency}</td>
+//                 <td>${elem.mid}</td>`
+//                 document.querySelector('table tbody').appendChild(tr)
+//             });
+//         })
+// }
+// const btn = document.querySelector('button')
+// btn.addEventListener('click', getData)
+
+
+//                  GIT
+
+// git init, git add . , git commit -m"" ,
+// komedy do kita gałęzie
+// git branch (sprawdza)
+// git branch -d nazwa(usuwa)
+// git branch nazwa (dodaje)
+// git checkout -b dev(zmienia i tworzy)
